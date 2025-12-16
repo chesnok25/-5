6109f511511733710a65d1db53682e5feced161f
Egor <chesnoke25@mail.ru>
2025-12-16 15:39:04 +0300

    юююю

--- src/src/test_calculator.py
+++ src/src/test_calculator.py
@@ -1,8 +1,6 @@
 import unittest
 import sys
 import os
-
-# Добавляем src в путь для импорта
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
 from calculator import Calculator
